3040e57f76420dfcfc94a57879729c8bd1ff6ee3
Dmitry Ledentsov <dmlled@yahoo.com>
2014-07-26 19:37:52 +0200

    background ticker ok

--- 4 newviewmodels.test/BackgroundTickerTests.cs
+++ 4 newviewmodels.test/BackgroundTickerTests.cs
@@ -5,6 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 using wpfrxexample.ViewModels;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Reactive.Testing;
+using ReactiveUI.Testing;
+using FluentAssertions;
+
 namespace wpfrxexample.ViewModels.Tests
 {
     [TestClass]
@@ -13,7 +17,19 @@ namespace wpfrxexample.ViewModels.Tests
         [TestMethod]
         public void BackgroundTickerTest()
         {
-            Assert.Fail();
+            (new TestScheduler()).With(scheduler =>
+            {
+                var ticker = new BackgroundTicker(scheduler);
+
+                int count = 0;
+                ticker.Ticker.Subscribe(_ => count++);
+
+                scheduler.AdvanceByMs(1000);
+                count.Should().Be(1);
+
+                scheduler.AdvanceByMs(2000);
+                count.Should().Be(3);
+            });
         }
     }
 }
