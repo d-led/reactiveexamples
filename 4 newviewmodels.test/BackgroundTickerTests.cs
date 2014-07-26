22b7a9a13076a5fc24416cac60d370c8ddd7abe1
Dmitry Ledentsov <dmlled@yahoo.com>
2014-07-26 19:33:00 +0200

    failing test

---
+++ 4 newviewmodels.test/BackgroundTickerTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wpfrxexample.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace wpfrxexample.ViewModels.Tests
+{
+    [TestClass]
+    public class BackgroundTickerTests
+    {
+        [TestMethod]
+        public void BackgroundTickerTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ 4 newviewmodels.test/WordCounterModelTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wpfrxexample.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace wpfrxexample.ViewModels.Tests
+{
+    [TestClass]
+    public class WordCounterModelTests
+    {
+        [TestMethod]
+        public void WordCounterModelTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
